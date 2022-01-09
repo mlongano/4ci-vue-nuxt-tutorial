8921a4a7eae3c15d44d96460d50e4784d988bd18
mlongano <mauro.longano@marconirovereto.it>
2022-01-09 18:28:50 +0000

    Extend plain JS example

--- vue-helloworld/js/app.js
+++ vue-helloworld/js/app.js
@@ -1,11 +1,22 @@
+if ( !String.prototype.interpolate ) {
+    String.prototype.interpolate = function ( o ) {
+        return this.replace(
+            /\{([^{}]*)\}/g,
+            function ( a, b ) {
+                var r = o[ b ];
+                return typeof r === 'string' || typeof r === 'number' ? r : a;
+            }
+        );
+    };
+}
+
 function data() {
         return {
-            message: 'Hello JS!!!'
+            message: 'Hello JS!!!',
+            world: 'JS'
         }
 }
 
 let messageElement = document.querySelector("#hello-vue");
-
-window.addEventListener('load', () => {
-    messageElement.textContent = data().message;
-})
+let content = messageElement.textContent.interpolate( data() );
+messageElement.textContent = content;
