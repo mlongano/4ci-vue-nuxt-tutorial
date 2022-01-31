de71e1f8e28133ac85314e542bd0554bae7115b1
mlongano <mauro.longano@marconirovereto.it>
2022-01-31 05:00:57 +0000

    Fetch from strapi

--- nuxt3-app/components/ArticleCard.vue
+++ nuxt3-app/components/ArticleCard.vue
@@ -1,9 +1,8 @@
 <template>
     <article class="m-4 md:w-1/2 lg:w-1/3">
-
-        <h2 class="mb-2 capitalize text-2xl font-semibold">{{ post.title }}</h2>
-      <p>{{ post.body }}...</p>
-    </article>
+        <h2 class="mb-2 capitalize text-2xl font-semibold">{{ post.Title }}</h2>
+        {{ post.Body }}
+  </article>
 </template>
 
 
@@ -11,6 +10,11 @@
     export default {
         props : {
             post: {}
-        }
+        },
+        computed : {
+            page: function() {
+             return `Hello ${this.$hello()}`
+            }
+        },
     }
 </script>
--- nuxt3-app/components/Articles.vue
+++ nuxt3-app/components/Articles.vue
@@ -3,13 +3,14 @@
     <ArticleCard
       v-for="post in posts"
       :key="post.id"
-      :post="post" />
+      :post="post.attributes" />
   </div>
 </template>
 
 <script setup>
     import ArticleCard from "./ArticleCard.vue";
     //const { data : posts } = await useFetch('https://jsonplaceholder.typicode.com/posts')
-    const  { data : {value: { data: posts } } } = await useFetch('http://localhost:8055/items/Articles')
+    const { data: { value : {data: posts } } } = await useFetch('http://localhost:1337/api/articles')
+    //const  { data : {value: { data: posts } } } = await useFetch('http://localhost:8055/items/Articles')
     console.log(posts);
 </script>
