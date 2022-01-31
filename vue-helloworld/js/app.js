69705f55fad7906a90932e69d0ecb42adc0c5633
mlongano <mauro.longano@marconirovereto.it>
2022-01-31 01:23:20 +0000

    Add test to Directus 9 api

--- vue-helloworld/js/app.js
+++ vue-helloworld/js/app.js
@@ -20,3 +20,13 @@ function data() {
 let messageElement = document.querySelector("#hello-vue");
 let content = messageElement.textContent.interpolate( data() );
 messageElement.textContent = content;
+
+async function pippo() {
+    let data1 = await fetch( 'http://localhost:8055/items/Articles' );
+    let data  = await data1.json()
+    let resp = { pippo : "pippo", data : data}
+    let { data : { data: posts } } = resp
+    console.log( posts );
+}
+
+pippo();
