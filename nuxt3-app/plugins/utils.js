604bf8a5510e8d14fafefb1bcad2c503c6bb2e57
mlongano <mauro.longano@marconirovereto.it>
2022-02-06 14:09:56 +0000

    Add image placeholder utils function

--- nuxt3-app/plugins/utils.js
+++ nuxt3-app/plugins/utils.js
@@ -3,7 +3,8 @@ import { defineNuxtPlugin } from '#app'
 export default defineNuxtPlugin( () => {
     return {
         provide: {
-            hello: () => 'world'
+            hello: () => 'world',
+            img_placeholder: () => 'https://source.unsplash.com/l3Jdvs1Qui4/800x600'
         }
     }
 } )
